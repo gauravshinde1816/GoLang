1fdc48289746940bd1eb594e103683c2e58c6ed2
gauravshinde1816 <gauravshinde1816@gmail.com>
2022-07-10 10:43:46 +0530

    initial commit

---
+++ GoLang101/helper/helper.go
@@ -0,0 +1,9 @@
+package helper
+
+import "fmt"
+
+func PrintHelperMessage() {
+
+	fmt.Printf("This is message from  helper package")
+
+}
---
+++ GoLang101/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func getName(name string) string {
+// 	return "hello" + name
+// }
+func main() {
+	// fmt.Println("Welcome to our conference booking application")
+	// helper.PrintHelperMessage()
+
+	// email, username := "gauravshinde@gmail.com", "gauravs"
+
+	// fmt.Print(getName("Gaurav"))
+
+	var fruitSlices = []string{"Apple", "Orange", "Grape", "Cherry"}
+	fmt.Print(fruitSlices)
+	fmt.Print(len(fruitSlices))
+	fmt.Print(fruitSlices[1:3])
+
+}
