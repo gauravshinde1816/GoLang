4b40da9c698002158c42f887f41c8f6135e71971
gauravs <gauravs@asiapac.altair.com>
2022-09-01 10:17:14 +0530

    basic server

--- simple-webserver-project-1/main.go
+++ simple-webserver-project-1/main.go
@@ -8,14 +8,14 @@ import (
 
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
-		http.Error(w, "404  page nt found", http.StatusNotFound)
+		http.Error(w, "404  page not found", http.StatusNotFound)
 		return
 	}
 
 	if r.Method != "GET" {
 		http.Error(w, "Request not supported", http.StatusNotFound)
 	}
-	fmt.Fprint(w, "Hello world")
+	fmt.Fprint(w, "<h1>Hello world</h1>")
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,14 +33,18 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// check static folder ==> looks for index.html by default
 	fileserver := http.FileServer(http.Dir("./static"))
 
 	http.Handle("/", fileserver)
 	http.HandleFunc("/hello", homePageHandler)
 	http.HandleFunc("/form", formHandler)
 
-	fmt.Print("Server i started on port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Print("Server is started on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
 		log.Fatal(err)
 	}
 
